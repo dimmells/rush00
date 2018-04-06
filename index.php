7d058d4f6a8d73653ed4b1b114b00e83da0d17df
Dmytro MELNYK <dmelnyk@e3r5p7.unit.ua>
2018-04-06 23:07:28 +0300

    Start! Good luck for us)

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>shop</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<a href="login.php">
+			<img class="login" src="img/login.png">
+		</a>
+		<a href="bascket.php">
+			<img class="bascket" src="img/basket.png">
+		</a>
+	</header>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="form_log">
+		<p class="p_sign">Sign In</p>
+		<input type="text" name="login">
+		<br>
+		<input type="password" name="passwd">
+		<input type="submit" name="submit" value="SignIn">
+	</div>
+</body>
+</html>
